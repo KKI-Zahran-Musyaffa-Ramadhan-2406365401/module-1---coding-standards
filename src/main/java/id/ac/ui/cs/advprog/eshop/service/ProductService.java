0eee8ba81cc2504b62614796d4aeffaa0a38fec0
zahran-musyaffa <zahran.musyaffa@ui.ac.id>
2026-02-24 13:32:37 +0700

    Refactor: Remove unnecessary public modifiers in ProductService interface

--- src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -4,14 +4,14 @@ import id.ac.ui.cs.advprog.eshop.model.Product;
 import java.util.List;
 
 public interface ProductService {
-    public Product create(Product product);
+    Product create(Product product);
 
-    public List<Product> findAll();
+    List<Product> findAll();
 
-    public Product findById(String productId);
+    Product findById(String productId);
 
-    public Product update(String productId, Product product);
+    Product update(String productId, Product product);
 
-    public void delete(String productId);
+    void delete(String productId);
 
 }
